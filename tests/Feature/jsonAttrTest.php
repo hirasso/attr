2dc8aa3e77fdc1dba7ff36f82dec75ce221a7566
Rasso Hilber <mail@rassohilber.com>
2026-01-18 10:00:32 +0100

    Add tests for correctly escaped json

--- tests/Feature/jsonAttrTest.php
+++ tests/Feature/jsonAttrTest.php
@@ -1,13 +1,31 @@
 <?php
 
-test('generates an escaped json attribute from an array', function () {
-    $result = jsonAttr(['foo' => 'bar', 'bool' => true, 'int' => 2, 'float' => 1.2, 'numeric' => '2.5']);
-    expect($result)->toBe('{&quot;foo&quot;:&quot;bar&quot;,&quot;bool&quot;:true,&quot;int&quot;:2,&quot;float&quot;:1.2,&quot;numeric&quot;:2.5}');
+test('generates a valid json attribute from an array', function () {
+    $result = jsonAttr([
+        'foo' => 'bar',
+        'bool' => true,
+        'int' => 2,
+        'float' => 1.2,
+        'numeric' => '2.5',
+        'xss' => '<script>alert("test")</script>',
+        'amp' => 'a & b',
+        'apos' => "it's",
+    ]);
+    expect($result)->toBe('{"foo":"bar","bool":true,"int":2,"float":1.2,"numeric":"2.5","xss":"\u003Cscript\u003Ealert(\u0022test\u0022)\u003C/script\u003E","amp":"a \u0026 b","apos":"it\u0027s"}');
 });
 
 test('generates an escaped json attribute from an object', function () {
-    $result = jsonAttr((object) ['foo' => 'bar', 'bool' => true, 'int' => 2, 'float' => 1.2, 'numeric' => '2.5']);
-    expect($result)->toBe('{&quot;foo&quot;:&quot;bar&quot;,&quot;bool&quot;:true,&quot;int&quot;:2,&quot;float&quot;:1.2,&quot;numeric&quot;:2.5}');
+    $result = jsonAttr((object) [
+        'foo' => 'bar',
+        'bool' => true,
+        'int' => 2,
+        'float' => 1.2,
+        'numeric' => '2.5',
+        'xss' => '<script>alert("test")</script>',
+        'amp' => 'a & b',
+        'apos' => "it's",
+    ]);
+    expect($result)->toBe('{"foo":"bar","bool":true,"int":2,"float":1.2,"numeric":"2.5","xss":"\u003Cscript\u003Ealert(\u0022test\u0022)\u003C/script\u003E","amp":"a \u0026 b","apos":"it\u0027s"}');
 });
 
 test('returns an empty string if the value is empty', function () {
