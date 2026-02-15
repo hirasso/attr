ea8f5338795c2a60dee3e404316023c173878c0f
Rasso Hilber <mail@rassohilber.com>
2026-02-15 22:29:45 +0100

    Format and lint test files as well

--- tests/Feature/JsonAttrTest.php
+++ tests/Feature/JsonAttrTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 \test('generates a valid json attribute from an array', function () {
     $result = \jsonAttr([
         'foo' => 'bar',
@@ -11,7 +13,9 @@
         'amp' => 'a & b',
         'apos' => "it's",
     ]);
-    \expect($result)->toBe('{&quot;foo&quot;:&quot;bar&quot;,&quot;bool&quot;:true,&quot;int&quot;:2,&quot;float&quot;:1.2,&quot;numeric&quot;:&quot;2.5&quot;,&quot;xss&quot;:&quot;&lt;script&gt;alert(\&quot;test\&quot;)&lt;/script&gt;&quot;,&quot;amp&quot;:&quot;a &amp; b&quot;,&quot;apos&quot;:&quot;it&apos;s&quot;}');
+    \expect($result)->toBe(
+        '{&quot;foo&quot;:&quot;bar&quot;,&quot;bool&quot;:true,&quot;int&quot;:2,&quot;float&quot;:1.2,&quot;numeric&quot;:&quot;2.5&quot;,&quot;xss&quot;:&quot;&lt;script&gt;alert(\&quot;test\&quot;)&lt;/script&gt;&quot;,&quot;amp&quot;:&quot;a &amp; b&quot;,&quot;apos&quot;:&quot;it&apos;s&quot;}',
+    );
 });
 
 \test('generates a valid json attribute from an object', function () {
@@ -25,13 +29,15 @@
         'amp' => 'a & b',
         'apos' => "it's",
     ]);
-    \expect($result)->toBe('{&quot;foo&quot;:&quot;bar&quot;,&quot;bool&quot;:true,&quot;int&quot;:2,&quot;float&quot;:1.2,&quot;numeric&quot;:&quot;2.5&quot;,&quot;xss&quot;:&quot;&lt;script&gt;alert(\&quot;test\&quot;)&lt;/script&gt;&quot;,&quot;amp&quot;:&quot;a &amp; b&quot;,&quot;apos&quot;:&quot;it&apos;s&quot;}');
+    \expect($result)->toBe(
+        '{&quot;foo&quot;:&quot;bar&quot;,&quot;bool&quot;:true,&quot;int&quot;:2,&quot;float&quot;:1.2,&quot;numeric&quot;:&quot;2.5&quot;,&quot;xss&quot;:&quot;&lt;script&gt;alert(\&quot;test\&quot;)&lt;/script&gt;&quot;,&quot;amp&quot;:&quot;a &amp; b&quot;,&quot;apos&quot;:&quot;it&apos;s&quot;}',
+    );
 });
 
 \test('returns an empty string if the value is empty', function () {
-    \expect(\jsonAttr([]))->toBe('');
-    \expect(\jsonAttr(null))->toBe('');
-    \expect(\jsonAttr(false))->toBe('');
+    \expect(\jsonAttr([]))->toBeEmpty();
+    \expect(\jsonAttr(null))->toBeEmpty();
+    \expect(\jsonAttr(false))->toBeEmpty();
 });
 
 \test('does not double-encode values', function () {
